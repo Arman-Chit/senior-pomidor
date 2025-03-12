b81bca48ac662a587aaee69d47d3145405f3243d
Arman-Chit <AChitoyan@yandex.ru>
2025-03-13 00:22:56 +0300

    вторая часть задания

---
+++ HomeWork3/ConditionalConstructions.py
@@ -0,0 +1,48 @@
+# Задача 1
+score = int(input("Введите ваш балл: "))
+def check_score(score):
+    if score >= 90 and score <= 100:
+        print(f"Оценка за {score} баллов: Отлично")
+    elif score >= 75 and score <= 89:
+        print(f"Оценка за {score} баллов: Хорошо")
+    elif score >= 50 and score <= 74:
+        print(f"Оценка за {score} баллов: Удовлетворительно")
+    else:
+        print(f"Оценка за {score} баллов: Неудовлетворительно")
+check_score(score)
+
+
+# Задача 2
+number = int(input("Введите число: "))
+print(f"Число {number} является четным" if number % 2 == 0 else f"Число {number} является нечетным")
+
+# Задача 3
+num1 = int(input("Введите первое число: "))
+num2 = int(input("Введите второе число: "))
+def find_max(num1, num2):
+    if num1>num2:
+        return num1
+    else:
+        return num2
+max_number = find_max(num1,num2)
+print(f"Максимальное из чисел {num1} и {num2}: {max_number}")
+
+# Задача 4
+number = int(input("Введите число: "))
+def check_number(number):
+    if number > 0:
+        if number % 2==0:
+            print(f"Число {number} положительное и чётное.")
+    else:
+        print(f"Число {number} отрицательное")
+check_number(number)
+
+#Задача 5
+user_string = input("Введите строку: ")
+user_length = int(input("Введите длину для проверки: "))
+def check_string_length(string, length):
+    if len(string) > length:
+        print(f'Длина строки "{user_string}" достаточная.')
+    else:
+        print(f'Строка "{user_string}" слишком короткая.')
+check_string_length(user_string, user_length)
---
+++ HomeWork3/TaskFunctions.py
@@ -0,0 +1,40 @@
+#Задача 1
+w= int(input("Введите длину прямоугольника: "))
+h= int(input("Введите ширину прямоугольника: "))
+
+def rectangle_area(width, height):
+    return width * height
+print(f"Площадь прямоугольника с шириной {w} и длиной {h} равна: {rectangle_area(w, h)}")
+
+
+#Задача 2
+seconds = int(input("Введите количество секунд: "))
+def convert_seconds(seconds):
+    hours = seconds//3600
+    minutes = (seconds % 3600) // 60
+    remaining_seconds = seconds % 60
+    return hours, minutes, remaining_seconds
+hours, minutes, remaining_seconds = convert_seconds(seconds)
+print(f'В {seconds} секундах содержится {hours} час(ов) и {minutes} минут(ы)')
+
+Задача 3
+number = int(input("Введите число: "))
+exponent = int(input("Введите степень: "))
+
+def power_of(number, exponent):
+    return number ** exponent
+print(f'Число {number} в степени {exponent} равно {power_of(number, exponent)} ')
+
+#Задача 4
+def count_items(*args):
+    count =len(args)
+    print(f'Количество переданнных элементов: {count}.')
+    return count
+count_items(1, 2, 3, 4, 5)
+count_items("apple", "banana", "cherry")
+
+def count_items(*args):
+    count = len(args)
+    print(f"Количество переданных элементов: {count}")
+
+count_items(1,2,3,4,5)
