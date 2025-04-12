64046d54a95d4bf6dce89f440d84b0bd1bcf8108
Arman-Chit <AChitoyan@yandex.ru>
2025-04-12 10:06:41 +0300

    review

--- HomeWork5/test_ui.py
+++ HomeWork5/test_ui.py
@@ -12,15 +12,24 @@ def test_checkout_order(browser):
     checkout_page = CheckoutPage(page)
 
     login_page.login('standard_user', 'secret_sauce')
+    assert page.locator(".title").inner_text() == "Products", "Login failed"
+
     inventory_page.add_first_item_to_cart()
+    assert page.locator(".shopping_cart_link").is_visible(), "Item not added to cart"
+
     checkout_page.start_checkout()
+    assert page.locator("#first-name").is_visible(), "Checkout page not loaded"
+
     checkout_page.fill_checkout("Arman", "Arman", "123")
     checkout_page.contain_checkout()
     checkout_page.finish_checkout()
-
-    assert page.locator(".complete-header").inner_text() == "Thank you for your order!"
+    assert page.locator(".complete-header").inner_text() == "Thank you for your order!", "Order not completed"
 
     checkout_page.burger_button()
     checkout_page.logout_button()
+
+    assert page.url == "https://www.saucedemo.com/", "Logout failed"
+
+
 
 
